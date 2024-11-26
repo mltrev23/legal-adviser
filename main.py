a3a74a938da10f7ca29026e5e0573eea6d55d773
mltrev23 <trevor.dev23@gmail.com>
2024-11-26 10:13:02 -0600

    feat: to device

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # Use a pipeline as a high-level helper
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cpu'
 
 # Load the tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("Equall/Saul-7B-Instruct-v1")
-model = AutoModelForCausalLM.from_pretrained("Equall/Saul-7B-Instruct-v1")
+model = AutoModelForCausalLM.from_pretrained("Equall/Saul-7B-Instruct-v1").to(device)
 
 # Define the prompt
 prompt = """
@@ -24,15 +28,15 @@ A: arbitrary
 Q: The mark "Aswelly" for a taxi service. What is the type of mark?
 A: fanciful
 
-Q: {{text}} What is the type of mark?
+Q: {text} What is the type of mark?
 A:
 """
 
 # Prepare the input for the model
-input_ids = tokenizer(prompt.format(text = "The mark 'Aswelly' for a taxi service."), return_tensors="pt").input_ids
+input_ids = tokenizer(prompt.format(text = "The mark 'Aswelly' for a taxi service."), return_tensors="pt").input_ids.to(device)
 
 # Generate a response
-output = model.generate(input_ids, max_length=150, num_return_sequences=1)
+output = model.generate(input_ids, max_length=1000, num_return_sequences=1)
 
 # Decode the generated output
 result = tokenizer.decode(output[0], skip_special_tokens=True)
